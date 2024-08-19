1123762350ccd3ab33c1b2404ada0b50789c69c5
Zkn0wn <weezhenyi@gmail.com>
2024-08-19 10:31:09 +0800

    Speech Bubble & Gun Controller(Not Done) By ZY

---
+++ Assets/Scripts/SpeechBubble/SpeechBubbleSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SpeechBubbleSO : ScriptableObject
+{
+    public string[] speechBubbleText;
+    public float[] speechBubbleTimer;
+}
