f72141f9aa46b331409e959864b5daeb72f30f9e
Zekque <ongzhengbo.ozb@gmail.com>
2024-08-20 17:08:25 +0800

    Debuff

--- Assets/Scripts/Combat/Debuffs/WaterDebuff.cs
+++ Assets/Scripts/Combat/Debuffs/WaterDebuff.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[CreateAssetMenu(menuName = "Debuffs/Example")]
+[CreateAssetMenu(menuName = "Debuffs/Water")]
 public class WaterDebuff : IDebuff
 {
     public override void InflictDebuff(PlayerStats playerStats)
     {
         playerStats.movementSpeed *= 0.9f;
+        playerStats.attackCooldown += 0.25f;
     }
 
 
---
+++ Assets/Scripts/EnergyPlant/DebuffEnergyManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebuffEnergyManager : MonoBehaviour
+{
+    public IDebuff debuff;
+
+    public string Scene1;
+    public string Scene2;
+
+    private PlayerStats playerStats;
+
+    private void Start()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+    }
+
+    private void Update()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        if (currentScene == Scene1)
+        {
+            RemoveDebuff(debuff);
+        }
+        else if (currentScene == Scene2)
+        {
+            ApplyDebuff(debuff);
+        }
+    }
+
+    private void ApplyDebuff(IDebuff debuffToApply)
+    {
+        if (playerStats != null)
+        {
+            if (!playerStats.debuffs.Contains(debuffToApply))
+            {
+                playerStats.debuffs.Add(debuffToApply);
+            }
+        }
+    }
+
+    private void RemoveDebuff(IDebuff debuffToRemove)
+    {
+        if (playerStats != null)
+        {
+            if (playerStats.debuffs.Contains(debuffToRemove))
+            {
+                playerStats.debuffs.Remove(debuffToRemove);
+            }
+        }
+    }
+}
