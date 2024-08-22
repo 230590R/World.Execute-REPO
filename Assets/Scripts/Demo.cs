f1131ad16369204bc1e7e266778e91db196fef1c
Zkn0wn <weezhenyi@gmail.com>
2024-08-22 09:14:49 +0800

    New Updated Reset Convo Script By Zy

--- Assets/Scripts/Demo.cs
+++ Assets/Scripts/Demo.cs
@@ -6,12 +6,22 @@ using UnityEngine;
 
 public class Demo : MonoBehaviour
 {
-    [SerializeField] List<DialougeSO> dialougeSO2 = new List<DialougeSO>();
+    List<DialougeSO> dialougeSO2 = new List<DialougeSO>();
     static bool reset = true;
 
     private void Start()
     {
         if (!reset) return;
+
+        NPCDialougeTrigger[] allDialougeGO = GameObject.FindObjectsOfType<NPCDialougeTrigger>();
+
+        for (int i = 0; i < allDialougeGO.Length; i++)
+        {
+            for (int j = 0; j < allDialougeGO[i].dialougeSOs.Length; j++)
+            {
+                dialougeSO2.Add(allDialougeGO[i].dialougeSOs[j]);
+            }
+        }
 
         for (int i = 0; i < dialougeSO2.Count; i++)
         {
